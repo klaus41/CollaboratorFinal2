f5976d79f0de193e285ec25d2b328cc4b1e28040
Klaus Gaarde <klausgaarde@live.dk>
2016-11-16 16:13:31 +0100

    no message

--- WebAPI/Models/IdentityModels.cs
+++ WebAPI/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace WebAPI.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Collaborator", throwIfV1Schema: false)
         {
         }
         
---
+++ WebUI/Controllers/Navision/IndexController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+using WebUI.ServiceGateway;
+
+namespace WebUI.Controllers.Navision
+{
+    public class IndexController : Controller
+    {
+        ThemeGateway _themeGateway = new ThemeGateway();
+        SearchCriteriaGateway _scGateway = new SearchCriteriaGateway();
+        ThemeManagerModel tm;
+
+        // GET: Index
+        public ActionResult Index()
+        {
+            tm = new ThemeManagerModel();
+            tm.searchCriterias = _scGateway.GetSearchCriterias();
+            tm.themes = _themeGateway.GetThemes();
+
+            return View(tm);
+        }
+    }
+}
